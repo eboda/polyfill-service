1294232a9648b12aa341ee7e385ff2fc23c9cf9c
Camille TJHOA <camille@contract-live.com>
2017-03-31 00:14:26 -0700

    Polyfill prepend for DocumentFragment
    According to:
    - https://dom.spec.whatwg.org/#dom-parentnode-prepend
    - https://dom.spec.whatwg.org/#interface-parentnode
    - https://developer.mozilla.org/en-US/docs/Web/API/ParentNode/prepend
    
    Prepend is a method for ParentNode yet ParentNode is an interface
    which is implemented by Document, DocumentFragment and Element.
    So prepend should be polyfilled for DocumentFragment too.
    Polyfill provided by MozillaDeveloperNetwork confirms it.

--- polyfills/Element/prototype/prepend/polyfill.js
+++ polyfills/Element/prototype/prepend/polyfill.js
@@ -1,3 +1,6 @@
-Document.prototype.prepend = Element.prototype.prepend = function prepend() {
+// IE8 doesn't support DocumentFragment.prototype
+var DocumentFragmentPrototype = (window.DocumentFragment && window.DocumentFragment.prototype) || document.createDocumentFragment().constructor.prototype;
+
+Document.prototype.prepend = DocumentFragmentPrototype.prepend = Element.prototype.prepend = function prepend() {
 	this.insertBefore(_mutation(arguments), this.firstChild);
 };
